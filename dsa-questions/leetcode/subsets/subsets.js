5d820a63dae1e29447f83fd5eda5b9a4b105a28b
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2024-08-18 17:24:05 +0530

    Add Subset Problem

---
+++ dsa-questions/leetcode/subsets/subsets.js
@@ -0,0 +1,41 @@
+/**
+ * @see https://leetcode.com/problems/subsets/
+ */
+
+// TC O(2^N)
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+  const ans = []; // Store all the subsets here
+  helper(nums, 0, [], ans); // Start the recursive process
+  return ans;
+};
+
+/**
+ * Recursive helper function to generate subsets
+ *
+ * @param {number[]} nums - The input array of numbers
+ * @param {number} index - The current index in the array we're considering
+ * @param {number[]} combinationsFormedSoFar - The current subset being formed
+ * @param {number[][]} ans - The final array to store all subsets
+ */
+function helper(nums, index, combinationsFormedSoFar, ans) {
+  // Base case: If we've considered all elements
+  if (index === nums.length) {
+    // Note: In JavaScript, arrays are passed by reference
+    ans.push([...combinationsFormedSoFar]); // Add a copy of the current subset to ans
+    return;
+  }
+
+  // Recursive case 1: Do not pick the current element
+  helper(nums, index + 1, combinationsFormedSoFar, ans);
+
+  // Recursive case 2: Pick the current element
+  combinationsFormedSoFar.push(nums[index]); // Add the current element to the subset
+  helper(nums, index + 1, combinationsFormedSoFar, ans);
+
+  // Backtrack by removing the last element to restore the previous state
+  combinationsFormedSoFar.pop();
+}
