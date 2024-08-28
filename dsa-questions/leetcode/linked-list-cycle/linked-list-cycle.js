bad0f7b6a9f4769a43f98cbee7d0e9c0ac088737
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2024-08-28 16:26:53 +0530

    Add Linked List Cycle

---
+++ dsa-questions/leetcode/linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * @see https://leetcode.com/problems/linked-list-cycle/
+ */
+
+// Time Complexity: O(n)
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head,
+    fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+
+  return false;
+};
