ee70dd017ec8a20eb932ac312e7d7cb7138523a5
Mohit Kumar Toshniwal <mohitkrtoshniwal@gmail.com>
2025-07-06 18:16:49 +0530

    Update reduce.js

--- polyfills/array/reduce.js
+++ polyfills/array/reduce.js
@@ -29,3 +29,31 @@ const sumWithInitial = reduce(
 );
 
 console.log(sumWithInitial);
+
+
+/**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {U}
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  const noInitialValue = initialValue === undefined;
+
+  if (noInitialValue && this.length === 0) {
+    throw new TypeError("Reduce of empty array with no initial value");
+  }
+
+  let accumulator = noInitialValue ? this[0] : initialValue;
+  let startingIndex = noInitialValue ? 1 : 0;
+
+  for (let index = startingIndex; index < this.length; index++) {
+    let value = this[index];
+
+    if (value) { // to handle sparse arrays
+      accumulator = callbackFn(accumulator, value, index, this);
+    }
+  }
+
+  return accumulator;
+};
